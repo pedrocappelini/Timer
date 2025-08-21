4eda835618d05567e0d53d0cff89e83182b08576
pedrocappelini <pedrocappelini@yahoo.com>
2025-08-21 15:14:39 -0300

    format

--- src/renderer/src/components/InputField.jsx
+++ src/renderer/src/components/InputField.jsx
@@ -1,15 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 export default function InputField({ label, value, onChange, placeholder }) {
+  const [displayValue, setDisplayValue] = useState(value);
+
+  const handleBlur = (e) => {
+    if (displayValue == "") {
+      setDisplayValue(value);
+    }
+  };
+
+  const handleFocus = (e) => {
+    setDisplayValue("");
+  };
+
   const handleChange = (e) => {
     const inputValue = e.target.value;
 
     if (/^\d*$/.test(inputValue)) {
-      if (parseInt(inputValue, 10) < 60) {
+      if (inputValue === "" || parseInt(inputValue, 10) < 60) {
+        setDisplayValue(inputValue);
         onChange(e);
-      }
-      if (inputValue === "") {
-        onChange({ target: { value: 0 } });
       }
     }
   };
@@ -18,10 +28,12 @@ export default function InputField({ label, value, onChange, placeholder }) {
     <div className="text-3xl">
       <label className="text-white">{label}:</label>
       <input
-        className="w-20 bg-white text-blue-400 bg-opacity-20 rounded-lg px-3"
+        className="w-20 bg-white text-blue-400 bg-opacity-20 rounded-lg px-3 text-center"
         type="number"
-        value={value}
+        value={displayValue}
         onChange={handleChange}
+        onFocus={handleFocus}
+        onBlur={handleBlur}
         placeholder={placeholder}
       ></input>
     </div>
